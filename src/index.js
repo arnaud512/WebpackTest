097985f6223f16943b6ed5029db05af8274f56ab
Arnaud DUPUY <arnaud.dupuy@ext.leroymerlin.fr>
2018-02-22 13:32:30 +0100

    end asset

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import _ from 'lodash';
+import './style.css'
+import Logo from './logo.png';
+import Data from './data.xml';
 
 function component() {
   var element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack', Data.note.to], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div
+  var myLogo = new Image();
+  myLogo.src = Logo;
+  element.appendChild(myLogo);
+
+  console.log(Data);
+
   return element;
 }
 
