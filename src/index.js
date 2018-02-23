103a06cd69d473e08dbeda9863e9835a8ef9f2c7
Arnaud DUPUY <arnaud.dupuy@ext.leroymerlin.fr>
2018-02-23 13:07:55 +0100

    Hot Module Replacement

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import _ from 'lodash';
 import printMe from './print.js';
+import './styles.css';
 
 function component() {
   var element = document.createElement('div');
   var btn = document.createElement('button');
 
-  element.innerHTML = _.join(['Helloo', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
@@ -15,4 +16,14 @@ function component() {
   return element;
 }
 
-document.body.appendChild(component());
+let element = component(); // Store the element to re-render on print.js changes
+document.body.appendChild(element);
+
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Acceptiong the update printMe module!');
+    document.body.removeChild(elemement);
+    element = component(); // Re-render the "component" to update the click handler
+    document.body.appendChild(element);
+  })
+}
